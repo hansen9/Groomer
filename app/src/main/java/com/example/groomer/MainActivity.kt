18793055229824494a1c095db8ea27a354f00970
Hansen <tofvoid@gmail.com>
2022-03-29 11:11:38 +0700

    button CC connect to view CC

--- app/src/main/java/com/example/groomer/MainActivity.kt
+++ app/src/main/java/com/example/groomer/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.groomer
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.groomer.databinding.ActivityMainBinding
+import com.example.groomer.ui.completeCare.completeCareFragment
+import com.example.groomer.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +24,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val mFragmentManager = supportFragmentManager
+        val mHomeFragment = HomeFragment()
+        val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+        if (fragment !is HomeFragment) {
+            Log.d("MyFlexibleFragment", "Fragment Name :" + HomeFragment::class.java.simpleName)
+            mFragmentManager
+                .beginTransaction()
+                .add(R.id.container, mHomeFragment,
+                    HomeFragment::class.java.simpleName)
+                .commit()
+        }
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
@@ -32,4 +48,5 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
 }
--- app/src/main/java/com/example/groomer/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/groomer/ui/home/HomeFragment.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.groomer.R
 import com.example.groomer.databinding.FragmentHomeBinding
+import com.example.groomer.ui.completeCare.completeCareFragment
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment() , View.OnClickListener{
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
@@ -37,9 +40,27 @@ class HomeFragment : Fragment() {
         })
         return root
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnCompleteCate: Button = view.findViewById(R.id.btn_complete_care)
+        btnCompleteCate.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View){
+        if(v.id == R.id.btn_complete_care){
+            val mCompleteCFragment = completeCareFragment()
+            val mFragmentManager = fragmentManager as FragmentManager
+            mFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, mCompleteCFragment,
+                    completeCareFragment::class.java.simpleName)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
