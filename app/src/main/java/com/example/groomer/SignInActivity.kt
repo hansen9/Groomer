6f2c7c33f6ebf72b1a01d7711e0d2f93f529416b
FemiElice <if-18050@students.ithb.ac.id>
2022-04-06 18:34:03 +0700

    sign up, sign in dan home

--- app/src/main/java/com/example/groomer/SignInActivity.kt
+++ app/src/main/java/com/example/groomer/SignInActivity.kt
@@ -1,11 +1,37 @@
 package com.example.groomer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.FragmentManager
+import com.example.groomer.ui.home.HomeFragment
+import com.example.groomer.ui.services.completeCareFragment
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() , View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_sign_up)
+        btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveActivityMenu: Button = findViewById(R.id.btn_join)
+        btnMoveActivityMenu.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_sign_up ->{
+                val moveIntent = Intent(this@SignInActivity, SignUpActivity::class.java)
+                startActivity(moveIntent)
+            }
+            R.id.btn_join ->{
+                val moveIntentMainActivity = Intent(this@SignInActivity, MainActivity::class.java)
+                startActivity(moveIntentMainActivity)
+            }
+        }
     }
 }
