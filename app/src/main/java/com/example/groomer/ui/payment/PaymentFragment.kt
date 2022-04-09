62875da852946101ebca7bee95a49db942a71e82
Hansen <tofvoid@gmail.com>
2022-04-09 18:54:10 +0700

    fragment_payment

--- app/src/main/java/com/example/groomer/ui/payment/PaymentFragment.kt
+++ app/src/main/java/com/example/groomer/ui/payment/PaymentFragment.kt
@@ -5,12 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.groomer.R
+import com.example.groomer.databinding.FragmentNotificationsBinding
+import com.example.groomer.databinding.FragmentPaymentBinding
+import com.example.groomer.ui.notifications.NotificationsViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -19,23 +24,31 @@ private const val ARG_PARAM2 = "param2"
  */
 class PaymentFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var paymentModel: PaymentModel
+    private var _binding: FragmentPaymentBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_payment, container, false)
+        paymentModel =
+            ViewModelProvider(this).get(PaymentModel::class.java)
+
+        _binding = FragmentPaymentBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.petNamePay
+        paymentModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
     }
 
     companion object {
@@ -51,10 +64,7 @@ class PaymentFragment : Fragment() {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             PaymentFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
             }
     }
 }
