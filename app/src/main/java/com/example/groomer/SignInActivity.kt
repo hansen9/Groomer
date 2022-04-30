540fad0b9f3b611d1a7ae11614d7917ad64e54d0
Hansen <tofvoid@gmail.com>
2022-04-30 14:19:26 +0700

    signUp + signIn backend

--- app/src/main/java/com/example/groomer/SignInActivity.kt
+++ app/src/main/java/com/example/groomer/SignInActivity.kt
@@ -3,16 +3,32 @@ package com.example.groomer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import android.view.View
 import android.widget.Button
-import androidx.fragment.app.FragmentManager
-import com.example.groomer.ui.home.HomeFragment
-import com.example.groomer.ui.services.completeCareFragment
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignInActivity : AppCompatActivity() , View.OnClickListener {
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var userNameEdit: EditText
+    private lateinit var passwordEdit: EditText
+
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        mAuth = Firebase.auth
+        userNameEdit = findViewById(R.id.edit_username)
+        passwordEdit = findViewById(R.id.edit_password)
 
         val btnMoveActivity: Button = findViewById(R.id.btn_sign_up)
         btnMoveActivity.setOnClickListener(this)
@@ -25,13 +41,51 @@ class SignInActivity : AppCompatActivity() , View.OnClickListener {
     override fun onClick(v: View) {
         when(v.id){
             R.id.btn_sign_up ->{
+                userLogin()
                 val moveIntent = Intent(this@SignInActivity, SignUpActivity::class.java)
                 startActivity(moveIntent)
             }
             R.id.btn_join ->{
-                val moveIntentMainActivity = Intent(this@SignInActivity, MainActivity::class.java)
-                startActivity(moveIntentMainActivity)
+                userLogin()
+
             }
         }
+    }
+    private fun userLogin() {
+        val email = userNameEdit.getText().toString().trim()
+        val password = passwordEdit.getText().toString().trim()
+
+        if(email.isEmpty()){
+            userNameEdit.setError("Email is required")
+            userNameEdit.requestFocus()
+        }
+
+        if(password.isEmpty()){
+            passwordEdit.setError("Password is required")
+            passwordEdit.requestFocus()
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            userNameEdit.setError("Please enter a valid email")
+            userNameEdit.requestFocus()
+        }
+
+//        progressBar.setVisibility(View.VISIBLE)
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){ task->
+                if (task.isSuccessful){
+                    Log.d(TAG, "logIn success")
+                    val user = mAuth.currentUser
+                    val moveIntentMainActivity = Intent(this@SignInActivity, MainActivity::class.java)
+                    startActivity(moveIntentMainActivity)
+                }else{
+                    Log.w(TAG, "login failed", task.exception)
+                    Toast.makeText(baseContext, "authentication failed",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
     }
 }
--- app/src/main/java/com/example/groomer/SignUpActivity.kt
+++ app/src/main/java/com/example/groomer/SignUpActivity.kt
@@ -3,14 +3,46 @@ package com.example.groomer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() , View.OnClickListener {
+    private lateinit var userName: EditText
+    private lateinit var userEmail: EditText
+    private lateinit var userPassword: EditText
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var userDB: DatabaseReference
+    private lateinit var btnJoin: Button
+
+    companion object{
+        private const val TAG = "EmailPassword"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+
+        userDB = FirebaseDatabase.getInstance(
+            "https://groomer-ead4a-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        ).getReference("users")
+        mAuth = Firebase.auth
+
+        userName = findViewById(R.id.edit_username)
+        userEmail = findViewById(R.id.edit_email)
+        userPassword = findViewById(R.id.edit_password)
+
+        btnJoin = findViewById(R.id.btn_join)
+        btnJoin.setOnClickListener(this)
 
         val btnMoveActivity: Button = findViewById(R.id.btn_sign_in)
         btnMoveActivity.setOnClickListener(this)
@@ -22,7 +54,62 @@ class SignUpActivity : AppCompatActivity() , View.OnClickListener {
                 val moveIntent = Intent(this@SignUpActivity, SignInActivity::class.java)
                 startActivity(moveIntent)
             }
+            R.id.btn_join ->{
+                signUp()
+            }
         }
+
+    }
+
+    private fun signUp() {
+        val email = userEmail.getText().toString().trim()
+        val password = userPassword.getText().toString().trim()
+        val name = userName.getText().toString().trim()
+
+        if(name.isEmpty()){
+            userName.setError("name is required")
+            userName.requestFocus()
+        }
+
+        if(email.isEmpty()){
+            userEmail.setError("email is required")
+            userEmail.requestFocus()
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            userEmail.setError("please provide email")
+            userEmail.requestFocus()
+        }
+
+        if(password.isEmpty()){
+            userPassword.setError("password is required")
+            userPassword.requestFocus()
+        }
+
+        if(password.length < 6){
+            userPassword.setError("min length should be 6 characters")
+            userPassword.requestFocus()
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){task->
+                if(task.isSuccessful()){
+                    val  user = User(name,email)
+                    val id: String
+                    id  = userDB.push().getKey().toString()
+                    Log.d(TAG, "createUserWithEmail:success")
+                    userDB.child(id).setValue(user)
+//                    ProgressBar.setVisibility(View.GONE)
+                }else{
+                    Log.w(TAG, "createUserWithEmail:Failure", task.exception)
+                    Toast.makeText(
+                        this@SignUpActivity,
+                        "failed to register! Try Again!",
+                        Toast.LENGTH_LONG
+                    ).show()
+//                    ProgressBar.setVisibility(View.GONE)
+                }
+            }
     }
 
 }
