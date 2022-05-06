043de1a9f4752ab7268c3aed674290fec4a527a2
FemiElice <if-18050@students.ithb.ac.id>
2022-05-06 19:46:27 +0700

    update home

--- app/src/main/java/com/example/groomer/MainActivity.kt
+++ app/src/main/java/com/example/groomer/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.groomer.databinding.ActivityMainBinding
 import com.example.groomer.ui.home.HomeFragment
+import com.example.groomer.ui.profile.ProfileFragment
+import com.example.groomer.ui.review.AllReviewFragment
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,23 +17,43 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val firstFragment = HomeFragment()
+        val secondFragment =  AllReviewFragment()
+        val thirdFragment = ProfileFragment()
 
+        setCurrentFragment(firstFragment)
         val mFragmentManager = supportFragmentManager
         val mHomeFragment = HomeFragment()
         val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
 
-        if (fragment !is HomeFragment) {
-            Log.d("MyFlexibleFragment", "Fragment Name :" + HomeFragment::class.java.simpleName)
-            mFragmentManager
-                .beginTransaction()
-                .add(R.id.container, mHomeFragment,
-                    HomeFragment::class.java.simpleName)
-                .commit()
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+                    setCurrentFragment(firstFragment)
+                    mFragmentManager.beginTransaction().add(R.id.container, mHomeFragment, HomeFragment::class.java.simpleName).commit()
+                }
+                R.id.preview->setCurrentFragment(secondFragment)
+                R.id.account->setCurrentFragment(thirdFragment)
+            }
+            true
         }
 
+//        val mFragmentManager = supportFragmentManager
+//        val mHomeFragment = HomeFragment()
+//        val fragment = mFragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+//
+//        if (fragment !is HomeFragment) {
+//            Log.d("MyFlexibleFragment", "Fragment Name :" + HomeFragment::class.java.simpleName)
+//            mFragmentManager.beginTransaction().add(R.id.container, mHomeFragment, HomeFragment::class.java.simpleName).commit()
+//        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
 
 }
